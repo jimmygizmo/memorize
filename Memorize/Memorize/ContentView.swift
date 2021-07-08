413dbf66fb5f51998711aa9ee3261ff4159b69cf
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-07-07 21:45:50 -0700

    can now add and remove cards using text buttons. next will use images from Apple's great library of symbols for these buttons.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,27 +9,61 @@ import SwiftUI
 
 
 struct ContentView: View {
-    var deckIcons = [ "🚗", "🚜", "✈️", "⛵️", "🛸", "🏍", "🛻", "🚃", "🚲", "🚁", "🚐", "🚂",
+    var deckIcons = [ "🚗", "🚜", "✈️", "⛵️", "🛸", "🏍", "🛻", "🚂", "🚃", "🚲", "🚁", "🚐",
                       "🚓", "🛴", "🚤", "🚙", "🛶", "🚕", "🏎", "🚎", "🚀", "🛺", "🛼", "🚚",
                       "🛹", "🚌", "🛵", "🚒", "🛥", "🚑", "🚛", "🛷" ]  // Count: 32
+    @State var iconCount = 6
     
     var body: some View {
-        HStack {
-            // Using id: \.self is a temporary hack because String is not an identifiable so it
-            // really cannot be used in a ForEach like this. This won't work for the real app.
-            // This assigns the string itself as the identifier, id:. TODO: Research \.self
-            ForEach(deckIcons, id: \.self) { cardIcon in
-                CardView(iconCharacter: cardIcon, isFaceUp: true)
-            }
-        }
-        .padding(.horizontal)
+        VStack {
+            
+            HStack {  // ROW OF CARDS
+                // Using id: \.self is a temporary hack. TODO: This will be fixed as game logic evolves.
+                ForEach(deckIcons[ 0 ..< iconCount ], id: \.self) { cardIcon in
+                    CardView(iconCharacter: cardIcon, isFaceUp: true)
+                }
+            }  // HStack ROW OF CARDS
+            .padding(.horizontal)
+            
+            HStack {  // Button - Spacer - Button
+                removeButton
+                Spacer()
+                addButton
+            }  // HStack Button - Spacer - Button
+            // NOTE: The tutorial has the .padding on the HStack, but I prefer it on each button for now.
+//            .padding(.horizontal)
+            
+        }  // VStack ContentView
     }
-}
-
-
-//    CONTINUE:
-//    https://youtu.be/3lahkdHEhW8?t=2432
-//    Lecture 2 at 40:32
+    
+    var removeButton: some View {
+            Button(action: {
+                iconCount -= 1
+            }, label: {
+                VStack {
+                    Text("Remove")
+                    Text("Card")
+                }
+            })
+            .padding(.horizontal)
+    }  // var removeButton
+    
+    var addButton: some View {
+        Button(action: {
+            iconCount += 1
+        }, label: {
+            VStack {
+                Text("Add")
+                Text("Card")
+            }
+        })
+        .padding(.horizontal)
+    }  // var addButton
+    
+    
+    
+    
+}  // ContentView
 
 
 struct CardView: View {
@@ -40,7 +74,8 @@ struct CardView: View {
         ZStack {
             let cardShape = RoundedRectangle(cornerRadius: 20)
             
-            if isFaceUp {  // FRONT, face up
+            if isFaceUp {  //  |FRONT|, face up
+                
                 cardShape.fill().foregroundColor(.green)
                 
                 cardShape.stroke(lineWidth: 3).foregroundColor(.purple)
@@ -48,19 +83,22 @@ struct CardView: View {
                 Text(iconCharacter).font(.largeTitle)
                     .shadow(color: .black, radius: 28)
                     .shadow(color: .black, radius: 12)
-            } else {  // BACK, face down
+                
+            } else {  //  |BACK|, face down
+                
                 cardShape.fill().foregroundColor(.gray)
                 
                 cardShape.stroke(lineWidth: 3).foregroundColor(.purple)
+                
             }
             
-        }  // ZStack
+        }  // ZStack CardView
         .onTapGesture {
             isFaceUp = !isFaceUp
         }
         
     }
-}
+}  // CardView
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -68,8 +106,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
             .previewDevice("iPhone 12 mini")
 //            .previewDevice("iPhone 6s Plus")
-            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
-//            .preferredColorScheme(.light)
+//            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)  // Comment this out for .light, which is the default.
     }
 }
 
