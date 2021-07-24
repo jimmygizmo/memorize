f508f3a71f7bc2cd873252fc764177a91f0fb97d
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-07-24 13:06:03 -0700

    cleaned up all comments and notes for tutorial b - bluetooth ibeacon detector. remaining todos can be looked later: 1. test/calibrate for usage of mbeacon to correspond to Apple distace standards for the proximity categories. 2. change print() logging to NSLog or similar. For now moving back to CS193P and/or another tutorial C.

--- Memorize/Memorize/BleScanContentView.swift
+++ Memorize/Memorize/BleScanContentView.swift
@@ -5,33 +5,33 @@
 //  Created by Jimmy Gizmo on 7/21/21.
 //
 
-// This file is related to a supplemental tutorial.
+// This file is related to a supplemental tutorial, distinct from the CS193P course.
 // Related items in project will be named/noted/flagged somehow as: Tutorial C
 //
 // SwiftUI Tutorial: Build an iBeacon detector with object binding and custom modifiers
 // https://www.youtube.com/watch?v=lCNpEaZiKqU
 
-// Many comments are transcribed from the narrator's discussion during the tutorial video.
-// In some places the audio was hard to hear clearly, so the could be a few comments that
-// don't make perfect sense yet, but I know where those are and will revisit them.
-// I tend to progressively condense, remove and formalize comments throughout a project.
-
-// Combine framework allows us to tell when data has changed using a 'passthrough' something?!
-// NOTE: Tutorial is old. BindableObject has been replaced with ObservableObject. I will attempt
-// to adapt/update the tutorial code into something that works.
+// NOTE: Tutorial is old. BindableObject has been replaced with ObservableObject. The code here
+// now has been adapted to work with Swift 5.5. You must also put @Published decorator on
+// the var that is published. Also, the @ObservedObject decorator is needed on the observed
+// BeaconDetector object/instance where it is instantiated/initialized.
+// I believe all of these are part of Combine. They all go together with the new ObservableObject.
 import Combine
 
-// Detect beacons among other things
 import CoreLocation
 
 import SwiftUI
 
 
 // TODO: Look for a memory leak. I see memory usage slowly increasing, but have no explanation.
+// There is continuous log output, but I would not expect that to show up as RAM usage, but
+// this is an IOS app running in an iPhone, so I don't know for sure how log output storage usage
+// shows up. Neither do I know the common pitfalls of memory leaks in Swift yet. These are
+// important areas I will cover soon as they are common problems for any kind of app.
 
 
-// This is just so our logging can show a human readable proximity. There don't appear to be a
-// built-in string option, just rawValue integers, which effectively is what is matching here:
+// This is just so our logging can show a human readable proximity. There doesn't appear to be a
+// built-in string option, just rawValue integers.
 extension CLProximity {
     var stringValue: String {
         switch self {
@@ -46,34 +46,31 @@ extension CLProximity {
         default:
             return "[Warning: Unconfigured CLProximity value. Cannot convert to string.]"
             // This error has not occurred in basic testing so it looks like these are all
-            // the cases we need to cover (along with the case: no beacons at all returned.)
+            // the cases we need to cover (along with the case of no beacons at all returned.)
         }
     }
 }
 
 
-// TODO: Clarify/re-write the below as it was transcribed from hard-to-hear tutorial audio:
-// BeaconDetector class will be an ObservableObject and a the delegate for a CL Location Manager.
-// A requirement for working with delegates on IOS in UIKit is you must inherit from NSObject.
-// WIll monitor beacons for us and notify our ContentView when something changes.
-// UPDATE: Looks like BindableObject is totally deprecated. Replaced with: ObservableObject
-// Details of getting this tutorial to work with ObservableObject are TBD.
+// Narrator comments:
+// 1. BeaconDetector class will be an ObservableObject and a the delegate for a CLLocationManager.
+// 2. A requirement for working with delegates in IOS/UIKit is you must inherit from NSObject.
+
 class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
     
     // Below, didChange is referred to as a publisher.
     var didChange = PassthroughSubject<Void, Never>()  // This is the only thing needed to
     // conform to ObservableObject. This comes from Combine.
-    // "Sending nothing (Void) and will 'Never' error."
+    // Narrator: Args mean: "Sending nothing (Void) and will 'Never' error."
     
-    // LocationManager to be kept alive during the entire life of the app.
+    // Narrator: LocationManager to be kept alive during the entire life of the app.
     var locationManager: CLLocationManager?
     
     // The @Published decorator was the final piece needed to cause the View to update.
     // I could see that the data was updating, but not the view. Swift has changed since the
-    // time of the tutorial and there are three pieces that work together in the new way,
+    // time of the tutorial and there are three pieces that work together,
     // different from the time of the tut. These are: @Published, ObservableObject and
-    // @ObservedObject. See docs for how these all work together. All are part of Combine,
-    // I am pretty sure.
+    // @ObservedObject. See docs for how these all work together.
     @Published var lastDistance = CLProximity.unknown
     
     override init() {
@@ -87,9 +84,11 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
         // We assign ourselves to be its delegate so we are told when something happens
         locationManager?.delegate = self
         
-        // User will be asked for permission to use the location. TODO: Clarify when.
-        // (He says, when app is being run.) Clarify if it saves the permission etc.
-        // I know Info.plist can be involved with such permissions. Clarify.
+        // User will be asked for permission to use the location.
+        // It looks like the permission persists across re-installations/upgrades of the app,
+        // meaning I have pushed many new versions to the phone but was only ever asked for
+        // permission to use Bluetooth once. TODO: Delete the app, including all data. Prove
+        // theory that if you do this, you will be prompted again for permission. UPDATE HERE.
         locationManager?.requestWhenInUseAuthorization()
     }  // init
     
@@ -98,13 +97,13 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager,
         didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
-            print("beacon scanning is authorized")
+            print("beacon scanning is authorized")  // TODO: Implement proper logging
             // Can this device scan for beacons?
             if CLLocationManager.isMonitoringAvailable(
                 for: CLBeaconRegion.self) {
-                print("beacon scanning is available")
+                print("beacon scanning is available")  // TODO: Implement proper logging
                 if CLLocationManager.isRangingAvailable() {
-                    print("beacon ranging is available")
+                    print("beacon ranging is available")  // TODO: Implement proper logging
                     startScanning()
                 }  // isRangingAvailable
                 
@@ -126,7 +125,7 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
         //
         // major: CLBeaconMajorValue -- Example: brand/company, store location
         //
-        // minor: CLBeaconMinorValue -- Example: floor within store
+        // minor: CLBeaconMinorValue -- Example: floor within store, area on a campus
         //
         // uuid, major and minor must match in whatever app or device you are using to
         // transmit beacons with.
@@ -136,9 +135,9 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
         let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint,
             identifier: "MyBeacon")
         
-        print("scanning for beacons starting now")
+        print("scanning for beacons starting now")  // TODO: Implement proper logging
         locationManager?.startMonitoring(for: beaconRegion)
-        print("ranging of beacons starting now")
+        print("ranging of beacons starting now")  // TODO: Implement proper logging
         locationManager?.startRangingBeacons(satisfying: constraint)
     } // startScanning
     
@@ -147,10 +146,10 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
         // Just pull out the first beacon that got matched. TODO: Potential problems?
         if let beacon = beacons.first {
-            print("beacon ranged: ", beacon.proximity.stringValue)
+            print("beacon ranged: ", beacon.proximity.stringValue)  // TODO: Implement proper logging
             update(distance: beacon.proximity)
         } else {
-            print("No appropriate beacons could be detected.")
+            print("No appropriate beacons could be detected.")  // TODO: Implement proper logging
             // TODO: We might not want to call update here, more even better .. we need to be
             // able to indicate an additional state, which we can obviously see we have from
             // the behavior and that is: no beacon detected.
@@ -177,7 +176,7 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
     // When we find a beacon
     // Record the measured distance and then tell the views to repaint based off new data.
     func update(distance: CLProximity) {
-        print("BEACON update: ", distance.stringValue)
+        print("BEACON update: ", distance.stringValue)  // TODO: Implement proper logging
         lastDistance = distance  // the property we defined
         didChange.send(())  // immediately call our publisher to inform any Views observing us
     }  // update
@@ -207,31 +206,18 @@ struct BleScanContentView: View {
     
     // Tutorial specifies that we must use return in each else block, but I think this applies
     // to older Swift. I get no warning at all WITHOUT returns and so I will leave them off for.
-    // now. Eventually I will decide on my own best practices or conform to some team style,
-    // but the Stanford IOS course which is the main focus of this project/repo is emphasizing
-    // things like minimalism .. hence leaving things out that we KNOW swift easily determines
-    // at compile time. The ethic seems to be at using less code and knowing Swift's inference
-    // mechanisms well, which makes sense to me and makes more sense in a more formal language.
-    // Compare this mindset to that of the Python community, where being explicit is valued and
-    // encouraged more (hence code is a bit more wordy at times) .. BUT with Python being so
-    // flexible, being explicit can have more value and sometimes be required.
-    // I'm tending to like this about Swift. I like the type safety, but I like the idea
-    // of leaving a well understood and well designed language type (and other)
-    // inference system to do its work and have less code overall.
-    // Also, compared to python, Swift syntax can be a LOT more 'busy' in places, with
-    // the functional features mixed in, etc. .. it makes sense to find ways to
-    // minimize and leave out unnecessary syntax.
-    // MORE:
+    // now. The Stanford IOS course which is the main focus of this project/repo emphasizes
+    // minimalism .. meaning to leave out optional syntx that we KNOW swift easily infers
+    // at compile time.
     // The error seen in the tutorial, which I believe we no longer get because of changes to
-    // Swift is said to come from not using return before each of the Text() in all of the four
-    // else cases below .. is exactly:
+    // Swift, is this:
     //     "Function declares an opaque return type, but has no return statement ....."
-    //     The final part of the error text is not visible in the video, if there is any.
+    //     (The final part of the error text is not visible in the video, if there is any.)
     
     var body: some View {
         if detector.lastDistance == .immediate {
             // There is an implicit 'return' before each Text(). This was required in Swift of
-            // a year or two ago, but nowadays it is preferred to remove, common implied syntax.
+            // a year or two ago, but nowadays it is preferred to remove common implied syntax.
             Text("RIGHT HERE")
                 .modifier(BigText())
                 .background(Color.green)
@@ -258,14 +244,13 @@ struct BleScanContentView: View {
 
 // IMPORTANT CLARIFICATION OF ACTUAL CODE EXECUTION FLOW AND SYSTEM OPTIMIZATIONS
 // It looks like IN FACT, no beacon detection or ranging was taking place at all
-// until the data was attempted to be accessed .. so some compiler or internal magic
-// optimization is taking place or at the very least I need to understand some of the
-// CoreLocation internals a bit better.
+// until the data was attempted to be accessed, so some optimization is taking place.
+//
 // OBSERVATION: Our log output never occurred and hence no code was run for beacon detect
-// and ranging UNTIL we hooked up @ObservedObject and attempted to update the view.
+// and ranging UNTIL we hooked up @ObservedObject AND attempted to update the view.
 // How can we sum up what this means? Can we say that "Publishers don't publish until they
-// have a subscriber?" Certinaly this is a smart kind of optimization. It is probably
-// something like that happening. Power-hungry radio usage certainly needs such optimizations.
+// have a subscriber?" Certinaly this would be a smart kind of optimization. BLE radios use
+// some power and hence all optimization opportunities are likely taken.
 
 
 // Try wrapping the preview code in: #if DEBUG\n...\n#endif
