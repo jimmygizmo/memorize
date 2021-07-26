8e17f776b1293fe3bed2341256a73569e19e37ec
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-07-25 19:53:39 -0700

    some very basic usage of CoreBluetooth is working. I figured out the key places to adapt this UIKit tutorial to SwiftUI and most is working. Final piece is to update a view, but that will come later. Most of all that is needed is already in place.

--- Memorize/Memorize/BleConnectView.swift
+++ Memorize/Memorize/BleConnectView.swift
@@ -38,10 +38,79 @@ class BleEngine: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriphe
     var myPeripheral: CBPeripheral!
     
     
+    
+    // The @Published decorator was the final piece needed to cause the View to update.
+    // I could see that the data was updating, but not the view. Swift has changed since the
+    // time of the tutorial and there are three pieces that work together,
+    // different from the time of the tut. These are: @Published, ObservableObject and
+    // @ObservedObject. See docs for how these all work together.
+    @Published var classUsefulInt = 0
+    
+    //**********************************
+    // In the UIKit app of the tutorial, this is what we would be doing:
+    // In the viewDidLoad() function, add the following line to initialize
+    // the centralManager variable:
+    // centralManager = CBCentralManager(delegate: self, queue: nil)
+    //
+    // So, where do we do this in the SwiftUI version?
+    // Using the Beacon tut C for guidance ... Looks like init() is the place.
+    
+    
+    
+    override init() {
+        super.init()  // NSObject initializer
+        
+        centralManager = CBCentralManager()
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        // We assign ourselves to be its delegate so we are told when something happens
+        // ADAPTED FROM BEACON CODE - LOOKS LIKE THIS IS DONE IN THE INIT CALL ARGS. DISABLING.
+        //centralManager?.delegate = self
+        
+        
+        // centralManager?.requestWhenInUseAuthorization() // ADAPTING: NO EQUIVALENT
+        
+    }  // init
+    
+    
+    
+    //**********************************
+    
+    
+    
+    
     // XCode provided this FIX stub so that we conform to the CBCentralManagerDelegate protocol.
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print("sup!")
+        if central.state == CBManagerState.poweredOn {
+            print("BLE powered on")
+        } else {
+            print("BLE is not powered on, not available or has some other problem.")
+        }
+//        CBManagerState.poweredOff
+//        CBManagerState.poweredOn
+//        CBManagerState.resetting
+//        CBManagerState.unauthorized
+//        CBManagerState.unknown
+//        CBManagerState.unsupported
     }  // centralManagerDidUpdateState
+    
+    // NICE! Everything working so far to just detect BLE is powered on.
+    
+    
+    
+    // When we want to update some data in the View. Perhaps an integer called usefulInt.
+    // This update will be called by some callback from the CoreBluetooth interactions.
+    // Look at BleBeaconContentView.swift for the parallel elements that need to be adapted.
+    // * I just made this an int to keep to keep it simple.
+    // * We will likely need a custom update method for every piece of information we want to
+    //   update separately in the UI.
+    func update(usefulInt: Int) {
+        // print("NOTICE update: ", usefulInt)  // Probably a good place to log.
+        classUsefulInt = usefulInt  // store it in the property we defined in this class.
+        didChange.send(())  // immediately call our publisher to inform any Views observing us
+    }  // update
     
     
 }  // BleEngine
