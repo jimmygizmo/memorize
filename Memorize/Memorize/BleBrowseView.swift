432c305ba24617c1fb622d3b7cc5156ddafa4501
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-08-03 15:55:44 -0700

    applied mostly the same modifiers to all the views and made it all close to what the tutorial does. made the 4 buttons have actions to log. made the list run off an array of test data. both light and dark modes currently look fine. works/looks good so far.

--- Memorize/Memorize/BleBrowseView.swift
+++ Memorize/Memorize/BleBrowseView.swift
@@ -12,8 +12,36 @@
 // The article mentions leaving out control of Peripheral advertising, so I will just add that
 // in myself if needed.
 
+// IDEA: I can see the near future where I want my apps UI to look and work equally well in both
+// light and dark modes. I've started researching/studying this. I might include that in this
+// file. I'm liking some of the techniques I see in this areticle:
+// https://thinkdiff.net/swiftui-dark-mode-the-easiest-way-81e48d055189
+// And this looks like a good and thorough article as well:
+// https://www.avanderlee.com/swift/dark-mode-support-ios/
+// One more:
+// https://blog.waldo.io/swiftui-dark-mode/
+// TODO: Dark mode links added to notes. Can be removed from here after looking closer at these.
+// Looks like not too much is needed unless you use/mix many colors in the UI. For default
+// backgrounds, the switching of text and probably also standard control colors would be
+// automatic.
+
 
 import SwiftUI
+
+
+struct Peripheral: Identifiable {
+    let name: String
+    //let uuid: UUID
+    //let rssi: Double?
+    let id = UUID()
+}
+
+private var peripherals = [
+    Peripheral(name: "BLE Peripheral 1"),
+    Peripheral(name: "BLE Peripheral 2"),
+    Peripheral(name: "BLE Peripheral 3"),
+    Peripheral(name: "BLE Peripheral 4")
+]
 
 
 struct BleBrowseView: View {
@@ -22,54 +50,74 @@ struct BleBrowseView: View {
     
     var body: some View {
         
-        VStack {  // - WINDOW
+        VStack (spacing: 10) {  // - WINDOW
             
-            Text("BLUETOOTH SCANNER")
-                .padding()
+            Text("Bluetooth Devices")
+                .font(.title)
+                .frame(maxWidth: .infinity, alignment: .center)
             
-            VStack {  // - PERIPHERALS
-                Text("Peripheral 1")
-                    .padding()
-                Text("Peripheral 2")
-                    .padding()
-                Text("Peripheral 3")
-                    .padding()
-            }  // VStack - PERIPHERALS
+            List(peripherals){  // - PERIPHERALS
+                Text($0.name)
+                    .italic()
+            }  // List - PERIPHERALS
+            .frame(height: 300)
+            
             
             Spacer()
             
-            VStack {  // - STATUS
-                Text("STATUS")
-                    .padding()
-                Text("Bluetooth is \(bluetoothStatus)")
-                    .padding()
-            }  // VStack - STATUS
+            Text("STATUS")
+                .font(.headline)
+            
+            Text("Bluetooth is \(bluetoothStatus)")
+                .foregroundColor(Color.red)
             
             
             HStack {  // - CONTROLS
-                VStack {
-                    Text("Start Scan")
-                        .padding()
-                    Text("Stop Scan")
-                        .padding()
+                VStack (spacing: 10) {
+                    Button(action: {
+                        print("Start scanning")
+                    }) {
+                        Text("Start Scan")
+                    }
+                    
+                    Button(action: {
+                        print("Stop scanning")
+                    }) {
+                        Text("Stop Scan")
+                    }
                 }
-                VStack {
-                    Text("Start Adv")
-                        .padding()
-                    Text("Stop Adv")
-                        .padding()
+                .padding()
+                
+                VStack (spacing: 10) {
+                    Button(action: {
+                        print("Start advertising")
+                    }) {
+                        Text("Start Adv")
+                    }
+                    
+                    Button(action: {
+                        print("Stop advertising")
+                    }) {
+                        Text("Stop Adv")
+                    }
                 }
+                .padding()
             }  // HStack - CONTROLS
+            
+            Spacer()
             
         }  // - WINDOW
         
-    }
+    }  // var body View
 }  // BleBrowseView
 
 
 struct BleBrowseView_Previews: PreviewProvider {
     static var previews: some View {
         BleBrowseView()
+            .previewDevice("iPhone 12 mini")
+//            .previewDevice("iPhone 6s Plus")
+            .preferredColorScheme(.dark)  // Comment this out for .light, which is the default.
     }
 }
 
