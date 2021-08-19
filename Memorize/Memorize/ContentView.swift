b0e2ddaf3393678eec07c10c3783e3453779cf47
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-08-19 13:38:58 -0700

    fixed many little positioning issues. fixed bug where deck count not set. added a little logging. cleaned up a LOT of comments and whitespace, making code more appropriate for corporate repositories. fixed title position by nudging down slightly. fixed theme button multiple little position issues.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,156 +8,110 @@
 import SwiftUI
 
 
-// TODO: Programming Assignment 1:
-// https://cs193p.sites.stanford.edu/sites/g/files/sbiybj16636/files/media/file/assignment_1.pdf
-
-
-/**********************************************************************************************************/
-// TODO: Multiple decks have multiple properties so later we might want to refactor and keep all
-// info for each deck together in an organized manner, such as with a struct along these lines.
-// This is not really needed if we only have 3 decks/themes but is a good pattern to consider.
-struct CardDeck {
-    var icons: [String]
-    let name: String
-    var count: Int {
-        icons.count
-    }
-    
-}
-// RELATED. Then we might:
-//var decks: [CardDeck] = []
-//var cardDeck = CardDeck(icons: [ "🍑", "🫐", "🍌", "🍕", "🫑",
-//                                 "🥪", "🍪", "☕️", "🍫", "🍚",
-//                                 "🌽", "🍒", "🍰", "🌭", "🍉", "🥑" ],
-//                        name: "Travel")
-/**********************************************************************************************************/
-
 let travelIcons = [ "🚗", "🚜", "✈️", "⛵️", "🛸", "🛻", "🚂", "🚃", "🚲", "🚁", "🛴", "🚤",
                   "🚙", "🚕", "🏎", "🚎", "🚀", "🛺", "🛼", "🚚", "🛹", "🚌", "🛵", "🚒",
-                  "🛥", "🚛" ]  // Count: 24
+                  "🛥", "🚛" ]  // count: 26
 
 let foodIcons = [ "🍑", "🫐", "🍌", "🍕", "🫑", "🥓", "🧁", "🌮", "🍗", "🍓", "🍦", "🍩",
                   "🥪", "🍪", "☕️", "🍚", "🍭", "🍔", "🥕", "🍍", "🫒", "🍒", "🍰", "🌭",
-                  "🍉", "🥖", "🥝", "🥑" ]  // Count: 28
+                  "🍉", "🥖", "🥝", "🥑" ]  // count: 28
 
 let animalIcons = [ "🐇", "🦞", "🦦", "🐏", "🪰", "🦩", "🦉", "🐟", "🪲", "🐳", "🐈", "🐀",
                   "🦋", "🐥", "🐗", "🐓", "🐿", "🐖", "🦢", "🐊", "🐅", "🐢", "🐸", "🐘",
-                  "🦝", "🦧", "🦃", "🦂", "🦀", "🦜", "🦥", "🦙" ]  // Count: 32
+                  "🦝", "🦧", "🦃", "🦂", "🦀", "🦜", "🦥", "🦙" ]  // count: 32
 
 
 struct ContentView: View {
-    
     @State var deckIcons = travelIcons.shuffled()
-    
     @State var iconCount = travelIcons.count
     
     var body: some View {
-        
         VStack {
             
-            Text("Memorize!")
+            Text("Memorize!")  // TODO: Try to make all font sizes absolute. Best practice?
+                .offset(y: 8)  // Most/all devices look better with title nudged down slightly.
                 .font(Font.custom("AmericanTypewriter-Bold", size: 24.0))
                 .foregroundColor(Color.purple)
-            // TODO: Make the font size relative and not absolute. It seems I was required to
-            // specify an absolute size!?
             
-            // TODO: The padding or spacing under this title, before the ScrollView starts
-            // (as shown by blue box/lines in preview when highlighted) is a bit too much and I
-            // would like to close that gap by about 30% less. It is not aparent what to adjust.
-            // It looks like the ScrollView is operating at it's full height with no padding,
-            // so I'm suspecting the Text item to be the issue. Almost looks like it is
-            // displaying an extra line with no text as the size matches for that.
-            // Line limit set to 1 had no effect and it should not have so that is good.
-            // I think it is some padding, spacing or framing associated with the Text view.
-            // TODO: Continue to look for a solution but it is OK for now. Minor issue.
-            
-            ScrollView {  // CARDS
+            ScrollView {
                 LazyVGrid(columns: [
                     GridItem(.adaptive(minimum: 65))
                 ]) {
-                    // For card aspect ratio, I use the inverse of the Golden Ratio: 1.618
-                    //
-                    // Using id: \.self is a temporary hack in place until we have game logic.
                     ForEach(deckIcons[ 0 ..< iconCount ], id: \.self) { cardIcon in
                         CardView(iconCharacter: cardIcon, isFaceUp: true)
                             .aspectRatio(0.618, contentMode: .fit)
                     }
-                }  // LazyVGrid
+                }
                 .padding(.horizontal)
                 .foregroundColor(.purple)
                 
-            }  // ScrollView - CARDS
+            }
                 
-                Spacer()
+            Spacer()
             
-            HStack {  // Button - Spacer - Button
+            HStack(alignment: .bottom) {
                 themeTravelButton
                 Spacer()
                 themeFoodButton
                 Spacer()
                 themeAnimalButton
-            }  // HStack Button - Spacer - Button
-            // NOTE: The tutorial has the .padding on the HStack.
-            // I prefer to keep this padding on the individual buttons for now.
-//            .padding(.horizontal)
-                
-        }  // VStack ContentView
-    } // var body View
+            }
+        }  // VStack
+    }
+    
     
     var themeTravelButton: some View {
         VStack {
             Button {
                 deckIcons = travelIcons.shuffled()
+                iconCount = travelIcons.count
+                print("Deck set to Travel and shuffled. Cards: \(iconCount)")
             } label: {
                 Image(systemName: "car")
             }
             .font(.largeTitle)
-            //.padding()
             
             Text("Travel")
                 .font(Font.custom("AmericanTypewriter-Bold", size: 16.0))
                 .foregroundColor(Color.purple)
-            //Spacer()
         }
-        //.frame(height: .leastNormalMagnitude)
-    }  // themeTravelButton
+        .padding(.horizontal, 20)
+    }
     
     var themeFoodButton: some View {
         VStack {
             Button {
                 deckIcons = foodIcons.shuffled()
+                iconCount = foodIcons.count
+                print("Deck set to Food and shuffled. Cards: \(iconCount)")
             } label: {
                 Image(systemName: "house")
             }
             .font(.largeTitle)
-            //.padding()
             
             Text("Food")
                 .font(Font.custom("AmericanTypewriter-Bold", size: 16.0))
                 .foregroundColor(Color.purple)
-            //Spacer()
         }
-        //.frame(height: .leastNormalMagnitude)
-    }  // themeFoodButton
+        .padding(.horizontal, 20)
+    }
     
     var themeAnimalButton: some View {
         VStack {
             Button {
                 deckIcons = animalIcons.shuffled()
+                iconCount = animalIcons.count
+                print("Deck set to Animals and shuffled. Cards: \(iconCount)")
             } label: {
                 Image(systemName: "person")
             }
             .font(.largeTitle)
-            //.padding()
-            
             Text("Animals")
                 .font(Font.custom("AmericanTypewriter-Bold", size: 16.0))
                 .foregroundColor(Color.purple)
-            //Spacer()
         }
-        //.frame(height: .leastNormalMagnitude)
-    }  // themeAnimalButton
-    
+        .padding(.horizontal, 20)
+    }
 }  // ContentView
 
 
@@ -169,30 +123,25 @@ struct CardView: View {
         ZStack {
             let cardShape = RoundedRectangle(cornerRadius: 20)
             
-            if isFaceUp {  //  |FRONT|, face up
-                
+            if isFaceUp {  //  ------------------------ |FRONT|, face up
                 cardShape.fill().foregroundColor(.green)
-                
                 cardShape.strokeBorder(lineWidth: 3)
                 
                 Text(iconCharacter).font(.largeTitle)
                     .shadow(color: .black, radius: 28)
                     .shadow(color: .black, radius: 12)
                 
-            } else {  //  |BACK|, face down
-                
+            } else {  //  ----------------------------- |BACK|, face down
                 cardShape.fill().foregroundColor(.gray)
-                
                 cardShape.strokeBorder(lineWidth: 3)
                 
             }
             
-        }  // ZStack CardView
+        }  // ZStack
         .onTapGesture {
             isFaceUp = !isFaceUp
         }
-        
-    }  // var Body View
+    }
 }  // CardView
 
 
@@ -204,6 +153,3 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)  // Comment this out for .light, which is the default.
     }
 }
-
-
-/**/
