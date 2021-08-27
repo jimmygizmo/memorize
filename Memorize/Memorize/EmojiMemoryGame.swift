0216191c6de0a779bae7232d51d6f2e82175ba5b
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-08-26 20:43:33 -0700

    still crafting initializers in the ViewModel and Model. this is an illustrative commit with a instance member property initializer error so it will not build. this commit is to follow along with the main steps in lecture 3.

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -9,26 +9,46 @@
 import SwiftUI
 
 
-// TODO: THIS WILL BE REMOVED. It is here to make an illustrative commit while following lecture 3.
-func makeCardSymbol(symbolIndex: Int) -> String {
-    return "🐠"
-}
+// WE COULD put this here:
+// let deckSymbols = [ "🍀", "☘️", ... ]
+// .. and it could be accessed before self BUT global vars are bad for many reasons. It is
+// very similar to being a global and can be accessed before any instances exists and also
+// it can be associated with it's namespace, IF we just make the property static.
+// We dont call these static though, we call them TYPE VARIABLES, TYPE FUNCTIONS and TYPE CONSTANTS.
+
+
+// AVAILABLE DECKS: deckWheels, deckFood, deckCritters, deckBeasts, deckPlants
+//let startingDeck = deckBeasts  // NOT USING THIS YET
+// This is named initialDeck in MemorizeApp.swift to resolve the re-declaration conflict. In a
+// real App there would not be two and a conflict, but we have code living side by side in this
+// project from multiple tutorials and multiple stages of CS193P, simultaneously.
 
 
 class EmojiMemoryGame {
-    // Becuse this is the Emoji version, the type for the MemoryGame generic is String:
-    private var model: MemoryGame<String> = MemoryGame<String>(pairsCount: 4,
-                                                               createCardSymbol: makeCardSymbol)
-    // NOTE: Because this is a class property, we MUST provide a value, here or in an init().
     
-    // AN ACCESSOR STRATEGY
-    // properties which you want to be private but want to allow reading of, could be made
-    // private(set), but you could also make it completely 'private' and then make another
-    // variable which is actually a little inline function, like this:
-    //     var cards: Array<MemoryGame<String>.Card {
-    //         return model.cards
-    //     }
-    // TODO: Comment here on how we use private(set) over in the model MemoryGame for 'cards'.
+    /* HOLD: Putting on hold, button-deck switching etc. to follow the lectures.
+    var deckName: String
+    var cardSymbols: [String]
+    var symbolCount: Int
+     */
+    
+    // Putting some emojis here like this in order to follow the lecture, but this will get
+    // more complex soon when we make the new MVVM version support button-deck-switching and
+    // deck names for logging etc. (Usage of Decks.swift)
+    static let deckSymbols = [ "🍀", "☘️", "🌷", "🍄", "🌹", "🌸", "🌻", "🌾", "🍁", "🌵",
+    "🌲", "🌳", "🌴", "🪴", "🌺", "🌼", "🍂", "🌿" ]
+    
+    func createMemoryGame() -> MemoryGame<String> {
+            MemoryGame<String>(pairsCount: 4) { pairIndex in
+            EmojiMemoryGame.deckSymbols[pairIndex]  // Just deckSymbols[pairIndex] works too.
+        }
+    }
+    
+    // Because this is the Emoji version, the type for the MemoryGame generic is String:
+    private var model: MemoryGame<String> = createMemoryGame()
+    // NOTE: Because this is a class property, we MUST provide a value, here or in an init().
+    // We have placeholders for the Int and the func with static value but that will change soon.
+
     
     // Remember Card is a struct so when we return this, the caller is getting its own copy.
     // Clearly this means it is read only as a result. Arrays are structs too for that matter.
@@ -36,6 +56,21 @@ class EmojiMemoryGame {
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
     }
+    
+    
+    //init() {
+        
+        /* HOLD: Putting on hold, button-deck switching etc. to follow the lectures.
+        //deckName = startingDeck.name
+        // MemoryGame will be initializing the cardSymbols array, so it is not yet clear how to
+        // resolve the need to initialize this property here, but I am jumping ahead of the lecture
+        // a bit and starting early to add in the button-deck-selection feature.
+        // Similarly, MemoryGame might also be setting symbolCount, but that is either the full
+        // array size or it is randomized. Not clear where that will happen in the new MVVM,
+        // but probably the buttons, then the initial init is separate, like in the non MVVM vers.
+         */
+        
+    //}  // init
     
     
 }  // EmojiMemoryGame
